3e6b406a4b2a35b98f11b895c869a72f1a89bd58
zhuhaihandachui <2512376800@qq.com>
2018-08-26 21:55:20 +0800

    carousel

--- menu/js/main.js
+++ menu/js/main.js
@@ -1,6 +1,26 @@
 $(document).ready(function(){
-	$('.bottom-nav > div > span').css({
-		'margin-left': -$('.bottom-nav > div > span').width()/2
-	});
+	$('.bottom-nav > div a').on('click', function(event) {
+		event.preventDefault();
+		$('.bottom-nav > div a').removeClass('active');
+		$(this).addClass('active');
 
+		if($(this).data('controls')=='task'){
+			$.ajax({
+				cache: false,
+				type: "get",
+				async: false,
+				url: "/index.html",
+				data: "",
+				success: function (data) {
+					$('#task').html(data);
+				},
+				error: function (xhr, ajaxOptions, thrownError) {
+					alert('請稍後重試.');
+				}
+			});
+
+		}else{
+			$('#task').html('');
+		}
+	});
 });
